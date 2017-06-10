e4ddaf56c0164edc7951a401d7ccbaacc9232452
dwleonhardt <dwleonhardt@gmail.com>
2017-06-10 11:55:40 -0600

    finished form and submission functionality, added script for form submission

---
+++ public/script/user_create.js
@@ -0,0 +1,43 @@
+
+$( document ).ready(function() {
+  function User(first_name, last_name, email, phone, age, photo_url){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.phone = phone;
+    this.age = age;
+    this.photo_url = photo_url;
+  }
+
+
+  $('.volunteer').on('submit', function(event){
+    event.preventDefault();
+
+    var newUser = new User();
+
+    newUser.first_name = $('#first_name').val();
+    newUser.last_name = $('#last_name').val();
+    newUser.email = $('#email').val();
+    newUser.phone = $('#phone').val() || null;
+    newUser.age = $('#age').val() || null;
+    newUser.photo_url = $('#photo_url').val() || null;
+
+    newUser = JSON.stringify(newUser);
+
+    var request = $.ajax({
+      url: "/users",
+      method: "POST",
+      data: newUser,
+      contentType: "application/json"
+    })
+    .done(function() {
+      alert( "Account Created" );
+    })
+    .fail(function() {
+      alert( "Email Already Exists" );
+    });
+
+
+
+  });
+});
