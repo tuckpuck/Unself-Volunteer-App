6af22faae306a83b63358d2f670e06ec0879671d
dwleonhardt <dwleonhardt@gmail.com>
2017-06-10 16:46:51 -0600

    troubleshooting heroku

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 app.use(bodyParser.json());
-app.use(express.static(path.join('public')));
+const path = require('path');
+app.use(express.static('public'));
 
 const users = require('./routes/users');
 const organizations = require('./routes/organizations');
