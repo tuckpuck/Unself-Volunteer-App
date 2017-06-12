f15c7c2aac595356823a6cad9670916ebb03b2e0
Tucker Triggs <tucker.triggs@yahoo.com>
2017-06-12 10:43:39 -0600

    fixed issue of description being a number, made organization email necessary, and got rid of hamburger menu on all pages

--- migrations/20170610124248_organizations.js
+++ migrations/20170610124248_organizations.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
       table.increments();
       table.string("name", 255).notNullable();
       table.string("phone", 25);
-      table.string("email", 255);
+      table.string("email", 255).notNullable();
       table.string("web_url", 500);
       table.string("description", 255);
       table.string("photo_url");
