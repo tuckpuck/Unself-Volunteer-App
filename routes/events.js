bad4dd1fb46c02100ee7cd3502f0748127531c77
Linda Call <lindacall4home@hotmail.com>
2017-06-13 17:15:29 -0600

    get organization events

--- routes/events.js
+++ routes/events.js
@@ -10,7 +10,6 @@ var knex = require('../knex');
 router.use(cookieParser());
 
 router.get('/events', function(req, res, next){
-  var newEvent = req.body;
   knex('events')
   .then(function(data){
     return res.send(data);
@@ -19,13 +18,17 @@ router.get('/events', function(req, res, next){
 
 
 router.get('/events/org', function(req, res, next){
-  var newEvent = req.body;
-  var tokenInfo = getTokenInfo(req);
-  if (tokenInfo) {
-    knex('events')
-    .where('organization_id', tokenInfo.organization_id)
-    .then(function(data){
-      return res.send(data);
+  if (req.cookies.token) {
+    jwt.verify(req.cookies.token, process.env.JWT_SECRET, function (err,decoded){
+      if (err) {
+        console.log(err);
+        return;
+      }
+      knex('events')
+      .where('organization_id', decoded.organization_id)
+      .then(function(data){
+        return res.send(data);
+      });
     });
   }
 });
@@ -33,38 +36,33 @@ router.get('/events/org', function(req, res, next){
 
 router.post('/events', function(req,res,next){
   var newEvent = req.body;
-  var tokenInfo = getTokenInfo(req);
-  if (tokenInfo) {
-    newEvent.organization_id = tokenInfo.organization_id;
-    knex('events')
-    .select()
-    .where('name', newEvent.name)
-    .then(function(data){
-      if(data.length > 0){
-        res.setHeader('Content-Type', 'text/plain');
-        return res.status(400).send('Event already exists');
-      }
-      knex('events')
-      .insert(newEvent, '*' )
-      .then(function(data){
-        var eventId = data[0].id;
-        res.cookie('newEvent', eventId, {httpOnly: true});
-        return res.send(data[0]);
-      });
-    });
-  }
-});
-
-function getTokenInfo(req) {
   if (req.cookies.token) {
     jwt.verify(req.cookies.token, process.env.JWT_SECRET, function (err,decoded){
       if (err) {
         console.log(err);
         return;
       }
-    return decoded;
+      newEvent.organization_id = decoded.organization_id;
+      knex('events')
+      .select()
+      .where('name', newEvent.name)
+      .then(function(data){
+        if(data.length > 0){
+          res.setHeader('Content-Type', 'text/plain');
+          return res.status(400).send('Event already exists');
+        }
+        knex('events')
+        .insert(newEvent, '*' )
+        .then(function(data){
+          var eventId = data[0].id;
+          res.cookie('newEvent', eventId, {httpOnly: true});
+          return res.send(data[0]);
+        });
+      });
     });
   }
-}
+});
+
+
 
 module.exports = router;
