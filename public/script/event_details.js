3e30da3a4b628a31170fb5c524c53e35f07a856a
dwleonhardt <dwleonhardt@gmail.com>
2017-06-14 11:54:20 -0600

    sending id through get route

---
+++ public/script/event_details.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var eventId = window.location.href.split('?')[1];
+  console.log(eventId);
+  var request = $.ajax({
+    url: `/event_roles:${eventId}`,
+    method: "GET",
+    contentType: "application/json"
+  })
+  .done(function(data){
+    console.log(data);
+  })
+  .fail(function(){
+    console.log('something went wrong');
+  });
+});
