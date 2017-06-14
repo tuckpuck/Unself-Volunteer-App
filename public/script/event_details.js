bc1ed3a391b40f512fd1b23b690ebf8c611acfd2
dwleonhardt <dwleonhardt@gmail.com>
2017-06-14 13:40:08 -0600

    added cards to event display

--- public/script/event_details.js
+++ public/script/event_details.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
   var eventId = window.location.href.split('?')[1];
-  console.log(eventId);
   var request = $.ajax({
     url: `/event_roles${eventId}`,
     method: "GET",
     contentType: "application/json"
   })
   .done(function(data){
-    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      console.log(data[i]);
+      var card = $(`<div class="card">
+      <div class="card-block">
+        <h5>${data[i].name}</h5>
+        <p>${data[i].description}</p>
+        <button type="button" class="btn btn-outline-success">Volunteer</button>
+      </div>
+      </div>`);
+      $('body').append(card);
+    }
   })
   .fail(function(){
     console.log('something went wrong');
