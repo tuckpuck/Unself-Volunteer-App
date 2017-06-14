233a89d8a018b4d035cc4a74479b06aacbcb4ada
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-06-14 17:15:46 -0600

    page redirects and keeps name on volunteer and org home pages

--- public/script/shared_js.js
+++ public/script/shared_js.js
@@ -3,7 +3,9 @@ $( document ).ready(function() {
   //clear cookie on logout
   $('#logout').on('click', function (event) {
     window.location.href = "logout.html";
-
+    localStorage.removeItem('name');
+    localStorage.removeItem('id');
+    localStorage.removeItem('origin');
     let request = $.ajax({
       url: "/token",
       method: "DELETE",
