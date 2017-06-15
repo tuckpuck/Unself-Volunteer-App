783a52cd80178d3cf0a1b9bd1f54a5c5c570c735
Linda Call <lindacall4home@hotmail.com>
2017-06-15 14:02:19 -0600

    fix problem with merge

--- public/script/event_details.js
+++ public/script/event_details.js
@@ -2,48 +2,48 @@ $(document).ready(function() {
   var eventId = window.location.href.split('?')[1];
   var userId = localStorage.getItem('id');
   var request = $.ajax({
-      url: `/event_roles${eventId}`,
-      method: "GET",
-      contentType: "application/json"
-    })
-    .done(function(data) {
-      console.log(data);
-      for (var i = 0; i < data.length; i++) {
-        console.log(data[i]);
-        $('#event_detail_img').attr('src', data[i].photo_url);
-        $('#event_detail_name').text(data[i].event_name);
-        $('#event_detail_org').text(data[i].organization_name);
-        if (data[i].start_date === data[i].end_date) {
-          $('#event_detail_date').text(`Date: ${data[i].start_date}`);
+    url: `/event_roles${eventId}`,
+    method: "GET",
+    contentType: "application/json"
+  })
+  .done(function(data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      console.log(data[i]);
+      $('#event_detail_img').attr('src', data[i].photo_url);
+      $('#event_detail_name').text(data[i].event_name);
+      $('#event_detail_org').text(data[i].organization_name);
+      if (data[i].start_date === data[i].end_date) {
+        $('#event_detail_date').text(`Date: ${data[i].start_date}`);
+      } else {
+        $('#event_detail_date').text(`Date: ${data[i].start_date} to ${data[i].end_date}`);
+      }
+      if (data[i].start_time === data[i].end_time) {
+        $('#event_detail_time').text(`Time: ${data[i].start_time}`);
+      } else {
+        $('#event_detail_time').text(`Time: ${data[i].start_time} to ${data[i].end_time}`);
+      }
+      $('#event_detail_description').text(data[i].event_description);
+      if (data[i].event_url.includes('www.')) {
+        $('#event_detail_url').attr('href', data[i].event_url).text('Visit the Website').attr('target', '_blank');
+      }
+      if (data[i].event_role_id !== null) {
+        var card = $(`<div class="card col-6">
+      <div class="card-block">
+        <h5>${data[i].name}</h5>
+        <p>${data[i].description}</p>
+        <button type="button" class="volunteer btn btn-outline-success" data-eventroleid=${data[i].event_role_id} data-userid=${userId}>Volunteer</button>
+      </div>
+      </div>`);
+        $('#event_detail_roles').append(card);
+
+        if (localStorage.getItem('origin') === 'org') {
+          $('.volunteer').hide();
         } else {
-          $('#event_detail_date').text(`Date: ${data[i].start_date} to ${data[i].end_date}`);
-        }
-        if (data[i].start_time === data[i].end_time) {
-          $('#event_detail_time').text(`Time: ${data[i].start_time}`);
-        } else {
-          $('#event_detail_time').text(`Time: ${data[i].start_time} to ${data[i].end_time}`);
-        }
-        $('#event_detail_description').text(data[i].event_description);
-        if (data[i].event_url.includes('www.')) {
-          $('#event_detail_url').attr('href', data[i].event_url).text('Visit the Website').attr('target', '_blank');
-        }
-        if (data[i].event_role_id !== null) {
-          var card = $(`<div class="card col-6">
-        <div class="card-block">
-          <h5>${data[i].name}</h5>
-          <p>${data[i].description}</p>
-          <button type="button" class="volunteer btn btn-outline-success" data-eventroleid=${data[i].event_role_id} data-userid=${userId}>Volunteer</button>
-        </div>
-        </div>`);
-          $('#event_detail_roles').append(card);
-          if (localStorage.getItem('origin') === 'org') {
-            $('.volunteer').hide();
-          } else {
-            $('.volunteer').show();
-          }
+          $('.volunteer').show();
         }
       }
-    });
+    }
 
     $('.volunteer').on('click', function(event){
       var eventRoleId = $(event.target).data('eventroleid');
@@ -59,15 +59,18 @@ $(document).ready(function() {
           method: "POST",
           data: newUserEventRole,
           contentType: "application/json"
-        })
-        .done(function(data) {
-          console.log(data);
-        });
+      })
+      .done(function(data) {
+        console.log(data);
+      })
+      .fail(function() {
+        console.log('no data');
       });
-    })
-    .fail(function() {
-      console.log('no data');
     });
+  });
+
+
+
   $('.event-details').on('click', function(event) {
     if (localStorage.getItem('origin') === 'org') {
       window.location.href = 'organization_home.html';
@@ -90,5 +93,4 @@ $(document).ready(function() {
         window.location.href = 'organization_home.html';
     }
   });
-
 });
